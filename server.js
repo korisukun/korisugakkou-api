8d4032d77092e728634b9185a9846a6a4cd0a8ef
korisukun <nuru.korisu@gmail.com>
2026-09-05 16:56:24 +0900

    Fix: Menambahkan izin CORS untuk domain Hostinger

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// Pengaturan Satpam CORS (Daftar Tamu VIP)
+app.use(cors({
+    origin: [
+        'https://korisugakkou.com', 
+        'https://www.korisugakkou.com',
+        'http://localhost:5000' // Tetap kita izinkan untuk tes di komputer lokal
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json()); // Menerima payload JSON dari frontend
 // Memanggil file routes
 const authRoutes = require('./src/routes/authRoutes');
